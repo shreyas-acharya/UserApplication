f709141b7b84d3839f3e3056c120ef52d84b1753
shreyas-acharya <acharyashreyas14@gmail.com>
2023-04-05 17:23:53 +0530

    Add VERSION file and build.sh file

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+USERNAME=shreyasacharyaa
+IMAGE=fastapi
+VERSION=`cat ./App/VERSION`
+echo "VERSION : $VERSION"
+
+docker build -t ghcr.io/$USERNAME/$IMAGE:$VERSION .
+docker tag ghcr.io/$USERNAME/$IMAGE:$VERSION ghcr.io/$USERNAME/$IMAGE:latest
+
+docker push ghcr.io/$USERNAME/$IMAGE:$VERSION
+docker push ghcr.io/$USERNAME/$IMAGE:latest
